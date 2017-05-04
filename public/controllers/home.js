bfb4d45cab2d5ae613657289cd3c3b82b10f3e8d
Jonathan <jonathan23@schule.bremen.de>
2017-05-04 23:15:03 +0200

    Final merge

--- public/controllers/home.js
+++ public/controllers/home.js
@@ -1,6 +1,16 @@
 angular.module('Zurival')
-    .controller('HomeCtrl', [
-        '$scope',
-        function($scope) {
-          $scope.title = 'Welcome to the end!';
+    .controller('HomeCtrl', ['$scope', 'authentication', function($scope, authentication) {
+
+            checkPlayBtnStatus();
+
+          $scope.title = 'Zurival - Welcome to the end!';
+
+          function checkPlayBtnStatus() {
+              if(authentication.isLoggedIn()) {
+                  $scope.playBtnLink = '.game.lobby';
+              }
+              else {
+                  $scope.playBtnLink = '.login';
+              }
+          }
     }]);
