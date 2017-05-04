33ce38ffaab40e9b0dee2af7463e46615bbb7c6e
Maik Beile <Maik Beile>
2017-05-04 22:47:39 +0200

    added commentary

--- public/directives/confirmPassword.js
+++ public/directives/confirmPassword.js
@@ -1,11 +1,13 @@
 angular.module('Zurival')
     .directive('confirmPassword', ['$parse', function($parse) {
         return {
+            // Restricts the directive only to be as an attribute.
             restrict: 'A',
             scope: {
                 matchTarget: '=',
             },
             require: 'ngModel',
+            // The link function is responsible for registering DOM listeners as well as updating the DOM.
             link: function link(scope, elem, attrs, ctrl) {
                 var validator = function (value) {
                     ctrl.$setValidity('match', value === scope.matchTarget);
